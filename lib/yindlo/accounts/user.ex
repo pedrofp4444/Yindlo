e1c00a46b6313b57630c780221ab62c0a2d493ff
pedrofp4444 <pfpereira2004@gmail.com>
2023-02-04 21:04:08 +0000

    Fix minor bug

--- lib/yindlo/accounts/user.ex
+++ lib/yindlo/accounts/user.ex
@@ -9,6 +9,7 @@ defmodule Yindlo.Accounts.User do
     field :password, :string, virtual: true, redact: true
     field :hashed_password, :string, redact: true
     field :confirmed_at, :naive_datetime
+
     field :role, Ecto.Enum, values: @roles
 
     timestamps()
